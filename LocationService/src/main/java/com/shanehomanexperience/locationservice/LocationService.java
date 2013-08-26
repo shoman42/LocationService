0db664112e6afdb054d0bc3df661b2fc53de02bf
shoman42 <shoman42@uw.edu>
2013-08-26 13:27:16 -0700

    .

--- LocationService/src/main/java/com/shanehomanexperience/locationservice/LocationService.java
+++ LocationService/src/main/java/com/shanehomanexperience/locationservice/LocationService.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Binder;
 import android.os.Bundle;
 import android.os.IBinder;
 
@@ -12,32 +13,15 @@ import android.os.IBinder;
  * Created by shoman42 on 8/23/13.
  */
 public class LocationService extends Service {
+    private final IBinder binder = new LocalBinder();
     private LocationManager lm;
+    private LocalLocationListener ll;
+
     @Override
     public void onCreate() {
         super.onCreate();
         lm = (LocationManager) getSystemService(LOCATION_SERVICE);
-        LocationListener ll = new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
-                newLocation(location);
-            }
-
-            @Override
-            public void onStatusChanged(String s, int i, Bundle bundle) {
-
-            }
-
-            @Override
-            public void onProviderEnabled(String s) {
-
-            }
-
-            @Override
-            public void onProviderDisabled(String s) {
-
-            }
-        };
+        ll = new LocalLocationListener();
     }
 
     void newLocation(Location location) {}
@@ -45,6 +29,7 @@ public class LocationService extends Service {
     @Override
     public void onDestroy() {
         super.onDestroy();
+        lm.removeUpdates(ll);
     }
 
     @Override
@@ -52,8 +37,36 @@ public class LocationService extends Service {
         return super.onStartCommand(intent, flags, startId);
     }
 
+    public class LocalBinder extends Binder {
+        LocationService getService() {
+            return LocationService.this;
+        }
+    }
+
+    public class LocalLocationListener implements LocationListener{
+        @Override
+        public void onLocationChanged(Location location) {
+            newLocation(location);
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
+
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        return binder;
     }
 }
